3b7e2bff21c85e004a2445645659d332949ddbb0
princessetowe <princessetowe@gmail.com>
2023-02-17 10:02:49 -0800

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-/*
- * main - This program prints from number 0 to 9.
+/**
+ * main - Prints numbers from 0 to 9
  *
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d", i);
-	}
-	putchar('\n');
+	printf("0123456789\n");
 	return (0);
 }
