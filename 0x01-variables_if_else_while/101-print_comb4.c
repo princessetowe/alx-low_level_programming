6fad208187cbce6fc23c80444ea24b1dd29a8b75
princessetowe <princessetowe@gmail.com>
2023-02-17 09:14:19 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ *main - This program paints numbers between 012 and 789.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+	i = 48;
+	j = 48;
+	k = 48;
+
+	while (j < 48)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			k = 48;
+			while (k < 58)
+			{
+				if (j != i && j != k && i != k && j < i && i < k)
+				{
+					putchar(j);
+					putchar(i);
+					putchar(k);
+					if (i == 56 && j == 55 && k == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
