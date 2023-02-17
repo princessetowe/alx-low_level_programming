eee4fd136ed23c0fb9926be2b351af57e9287079
princessetowe <106072527+princessetowe@users.noreply.github.com>
2023-02-17 17:19:37 +0100

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,13 +2,13 @@
 /**
  * main - This program prints numbers between 0 to 9 with commas and spaces
  *
- *Return : Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 
-	for (i =48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 		if (i != 57)
