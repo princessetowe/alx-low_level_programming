739f0869ca97702d09d78362b98b420d83666c44
princessetowe <princessetowe@gmail.com>
2023-02-21 03:30:49 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main- This program prints the alphabets in lower case
+ *
+ *Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
