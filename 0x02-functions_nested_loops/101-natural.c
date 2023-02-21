701892141b7ce80b067578b2bf99a8253f1d819d
princessetowe <princessetowe@gmail.com>
2023-02-21 14:52:40 -0800

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main- Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+	int sum = 0;
+
+	while (a < 1024)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			sum += a;
+		}
+		c++
+	}
+	printf("%n" , sum);
+	return (0);
+}
