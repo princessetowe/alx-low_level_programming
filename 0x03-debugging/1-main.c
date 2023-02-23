d251b888583fca859f4a2cb49733e1908c639521
princessetowe <106072527+princessetowe@users.noreply.github.com>
2023-02-23 18:49:48 +0100

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming : (\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
@@ -17,6 +17,6 @@ int main(void)
 	 *	putchar(i);
 	 *}
 	 */
-	printf("Infinitr loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
