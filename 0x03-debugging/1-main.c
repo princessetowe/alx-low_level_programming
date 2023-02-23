55ced25737c25cabff1faf31c998a58750090ef6
princessetowe <106072527+princessetowe@users.noreply.github.com>
2023-02-23 18:54:41 +0100

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  *
  *Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
@@ -12,10 +13,11 @@ int main(void)
 
 	i = 0;
 
-	/**while (i < 10)
-	 *{
+	/**
+	 * while (i < 10)
+	 * {
 	 *	putchar(i);
-	 *}
+	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
