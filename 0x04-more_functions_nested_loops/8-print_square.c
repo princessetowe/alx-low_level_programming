2db0824c1520787f0da819bcf1abed59d046886c
princessetowe <princessetowe@gmail.com>
2023-02-24 10:18:36 -0800

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_square - prints a square
+ *@size: number to be printed
+ *Return: no return
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar(35);
+		}
+		if (a != size - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
