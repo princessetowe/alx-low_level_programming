03a43cef908e76831cac9065d0f4d8b4a2ca0e19
princessetowe <106072527+princessetowe@users.noreply.github.com>
2023-02-24 02:43:38 +0100

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  *print_remaining_days - print remaining day
  *@month: month in number format
@@ -10,7 +9,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !year % 100 == 0)
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
 		if (month >= 2 && day >= 60)
 		{
