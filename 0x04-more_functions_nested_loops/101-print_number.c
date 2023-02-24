e081fc6aa03ce4db1332a4dfd0cab7efa51e26cf
princessetowe <princessetowe@gmail.com>
2023-02-24 11:46:03 -0800

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_number- prints integers
+ *@n: number
+ *Return: n return
+ */
+void print_number(int n)
+{
+	unsigned int a = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = -a;
+	}
+
+	if ((a / 10) > 0)
+	{
+		print_number(a / 10);
+	}
+	_putchar((a % 10) + '0');
+}
