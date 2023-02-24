9ba4995dd54562aa380f86a737edde8e7579cfc8
princessetowe <princessetowe@gmail.com>
2023-02-24 10:35:02 -0800

    Taask 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Return: no return
+ */
+int main(void)
+{
+	int a = 1;
+
+	while (a <= 100)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", a);
+		}
+		if (a != 100)
+		{
+			putchar(' ');
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
