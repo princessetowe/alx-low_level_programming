2da30d8b3db7471c80ad07297c9708ae3e0200a6
princessetowe <princessetowe@gmail.com>
2023-02-23 16:54:41 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - prints the numbers 0 to 9, followed by a new line
+ *Return: Always 0 
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
