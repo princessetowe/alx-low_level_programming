8e3976a6be7ea36df854358d15579b91f79856b1
princessetowe <princessetowe@gmail.com>
2023-02-27 15:23:32 -0800

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _atoi- converts a string to an integer
+ * @s: char type string
+ * Return: integer results
+ */
+void _atoi(char *s)
+{
+	int a;
+	int b = 0;
+	int c = -1;
+	int brk = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == '-')
+			c = c * -1;
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			b = b * 10;
+			b -= (s[a] - '0');
+			brk = 1;
+		}
+		else if (brk == 1)
+			break;
+	}
+	b = c * b;
+	return (b);
+}
