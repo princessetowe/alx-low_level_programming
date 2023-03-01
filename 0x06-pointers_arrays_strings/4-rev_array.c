a777d62e671a34bd205966eaa723ab120bc8f929
princessetowe <princessetowe@gmail.com>
2023-03-01 10:25:14 -0800

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array- reverse array of integer
+ * @a: array
+ * @n: numbers of elements in array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int b, c;
+
+	for (b = 0; b < n--; b++)
+	{
+		c = a[b];
+		a[b] = a[n];
+		a[n] = t;
+	}
+}
