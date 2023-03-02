8d1e0d51dc679242bc0aae1ab58399565b2e479e
princessetowe <princessetowe@gmail.com>
2023-03-02 08:08:00 -0800

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string- capitalizes all words of a string
+ * @s: input string
+ * Return: pointer
+ */
+char *cap_string(char *s)
+{
+	int a = 0, b;
+	int words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (s[a] >= 97 && s[a] <= 122)
+		s[a] = s[a] - 32;
+	a++;
+	while (s[a] != '\0')
+	{
+		for (b = 0; b < 13; b++)
+		{
+			if (s[a] == words[b])
+			{
+				if ((*(s + (a + 1)) >= 97) && (*(s + (a + 1)) <= 122)
+						*(s + (a + 1)) = *(s + (a + 1)) - 32;
+				break;
+			}
+		}
+		a++;
+	}
+	return (s);
+}						
