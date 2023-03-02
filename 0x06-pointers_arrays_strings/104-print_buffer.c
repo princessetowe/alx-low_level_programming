dd844bcb17b3961af4d419a513c63218101f9b3d
princessetowe <princessetowe@gmail.com>
2023-03-02 10:29:36 -0800

    Task 12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_buffer - printts a buffer
+ * @b: buffer.
+ * @size: size of buffer
+ * Return: none
+ */
+void print_buffer(char *b, int size)
+{
+	int a, b, c;
