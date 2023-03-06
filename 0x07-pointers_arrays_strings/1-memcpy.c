c5ce3a353ad405dceeaa2c3fa26aac4b3657c043
princessetowe <106072527+princessetowe@users.noreply.github.com>
2023-03-06 15:44:08 +0100

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  * @n: number of bytes
  * Return: copied memory with n bytes
  */
-char *memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
 	int i = n;
