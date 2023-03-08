19a198547967ec8b9367de14126cd9e32d8a9447
princessetowe <princessetowe@gmail.com>
2023-03-08 09:33:46 -0800

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Entry point
+ * @s: string to be measured
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
+}
