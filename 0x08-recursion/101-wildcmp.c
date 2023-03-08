a88c302e11d9263205261b82baee37125569b87c
princessetowe <princessetowe@gmail.com>
2023-03-08 10:47:38 -0800

    Task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * wildcmp - Entry point
+ * @s1: string one
+ * @s2: string two (can cotain special character (*))
+ * Return: 1 if both strings are identical, 0 if they are not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 ==  '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
