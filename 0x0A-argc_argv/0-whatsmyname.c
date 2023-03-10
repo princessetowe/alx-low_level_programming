f89848c1c42451c49d0c83e6f4226934e4831ecb
princessetowe <princessetowe@gmail.com>
2023-03-10 08:42:28 -0800

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: offset count
+ * @argv: offset value
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
