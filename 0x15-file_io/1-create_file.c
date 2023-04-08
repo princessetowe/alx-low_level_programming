d7a49fdc9fa5b11a660713e5ef413729f1136419
princessetowe <princessetowe@gmail.com>
2023-04-08 08:29:55 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: filename
+ * @text_content: content written in the file
+ * Return: 1 if successful -1 if it fails
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fn;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fn == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fn, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fn);
+
+	return (1);
+}
