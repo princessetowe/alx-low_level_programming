0d0b48f993ba4efc41dc058eaef54f8d88c15850
princessetowe <106072527+princessetowe@users.noreply.github.com>
2023-04-08 18:09:53 +0100

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,10 +1,8 @@
+#include <elf.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <elf.h>
 
 /**
  * print_addr - prints address
