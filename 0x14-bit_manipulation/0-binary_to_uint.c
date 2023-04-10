77ba7581cdf30dc2d88dc77951e8fb1c45b6ecf1
princessetowe <106072527+princessetowe@users.noreply.github.com>
2023-04-10 13:12:53 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,18 +7,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int u;
-	int len, base2;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
 
-	u = 0;
+	ui = 0;
 
 	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (len--; base2 = 1; len >= 0; len--; base2 *= 2)
+	for (len--; base_two = 1; len >= 0; len--; base_two *= 2)
 	{
 		if (b[len] != '\0' && b[len] != '1')
 		{
@@ -27,9 +27,9 @@ unsigned int binary_to_uint(const char *b)
 
 		if (b[len] & 1)
 		{
-			u += base2;
+			ui += base_two;
 		}
 	}
 
-	return (u);
+	return (ui);
 }
