b6d4a21267ad93c11c17fb0dbb334f78701835ea
princessetowe <princessetowe@gmail.com>
2023-04-10 05:06:20 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * binary_to_uint - cnverts a binary number to an unsigned int
+ * @b: binary number
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int u;
+	int len, base2;
+
+	if (!b)
+		return (0);
+
+	u = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--; base2 = 1; len >= 0; len--; base2 *= 2)
+	{
+		if (b[len] != '\0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			u += base2;
+		}
+	}
+
+	return (u);
+}
